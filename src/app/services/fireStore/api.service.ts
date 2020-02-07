4b3e7597618e1dd633256567b7225d40092c88f2
Alexander Yampolski <al.yampolski@gmail.com>
2020-02-07 12:37:46 +0200

    Add firebase-firestore to proj

--- src/app/services/fireStore/api.service.ts
+++ src/app/services/fireStore/api.service.ts
@@ -3,7 +3,7 @@ import { AngularFirestore, Action, DocumentSnapshot, DocumentChangeAction } from
 import { AngularFireAuth } from '@angular/fire/auth';
 import { Router } from '@angular/router';
 
-import * as firebase from 'firebase';
+// import * as firebase from 'firebase';
 import { switchMap, catchError, mergeMap } from 'rxjs/operators';
 import { from, Observable, of } from 'rxjs';
 import { throwError } from 'rxjs';
